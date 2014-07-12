47451af30b47d501739c4ea31e44c12619567b49
thannes30 <thannes30@yahoo.com>
2014-07-11 23:46:49 -0400

    added bootstrap

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,26 @@
-$(function(){
-  $("#nav a").click(function(e){
-    e.preventDefault();
-    $('html,body').scrollTo(this.hash,this.hash);
-  });
-});
+// $(function(){
+//   $("#nav a").click(function(e){
+//     e.preventDefault();
+//     $('html,body').scrollTo(this.hash,this.hash);
+//   });
+// });
+
+
+//active nav
+$(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            // e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+
+// $('.navbar-header li').click(function(e) {
+//     $('.navbar-header li.active').removeClass('active');
+//     var $this = $(this);
+//     if (!$this.hasClass('active')) {
+//         $this.addClass('active');
+//     }
+//     e.preventDefault();
+// });
+
