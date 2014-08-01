ee31566fcff6e90619ed672853aadb82350b7f41
thannes30 <thannes30@yahoo.com>
2014-08-01 18:16:43 -0400

    implemented lightbox, and pushed live

---
+++ javascripts/lightbox.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($){
+
+  $('.navigation').animate({'margin-top': '0px'}, 1000);
+
+  $(".lightbox_trigger").click(function(e){
+    e.preventDefault();
+    var image_href = $(this).attr("href");
+    if ($("#lightbox").length > 0){
+      $("#content").html('<img src="' +image_href+ '"/>');
+      $("#lightbox").show();
+    } else {
+      var lightbox =
+         '<div id="lightbox">'+
+            '<div id="content">'+
+              '<img src="' +image_href+ '"/>'+
+            '</div>'+
+          '</div>';
+        $('body').append(lightbox);
+    };
+
+     $("#lightbox").on('click', function(){
+       $('#lightbox').hide();
+     });
+
+  });
+});
